a97fd1f9db347b67977f571101007332787b3fa4
Pritam Biswas <Pritambiswas01@gmail.com>
2020-10-09 05:43:02 +0000

    groovy file 5

--- dsl_1.groovy
+++ dsl_1.groovy
@@ -1,6 +1,6 @@
 job('job_4 via dsl-plugin') {
     scm {
-        git("https://github.com/PritamBiswas01/Python2.git", master)
+        git('https://github.com/PritamBiswas01/Python2.git','master')
     }
     triggers {
         scm('* * * * *')
