efdc1c63e36d6f84df9fd51d3dc2069dc370a929
Pritam Biswas <Pritambiswas01@gmail.com>
2020-10-09 17:01:55 +0000

    n

--- dsl_2.groovy
+++ dsl_2.groovy
@@ -1,4 +1,10 @@
 job('job_5 via dsl-plugin') {
+    scm {
+        git("https://github.com/pritambiswas01/Python2.git", 'master')
+    }
+    triggers {
+        scm('* * * * *')
+    }
     steps {
         shell(('echo Hello how are u'))
     }
