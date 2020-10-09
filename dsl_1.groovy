244cdf93c7630f605b9e4c7df6550ff2267e7718
Pritam Biswas <Pritambiswas01@gmail.com>
2020-10-09 06:17:41 +0000

    hello

--- dsl_1.groovy
+++ dsl_1.groovy
@@ -1,13 +1,4 @@
 job('job_4 via dsl-plugin') {
-    scm {
-        git('https://github.com/PritamBiswas01/Python2.git','master'){ node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('me@me.com')
-        }
-    }
-    triggers {
-        scm('* * * * *')
-    }
     steps {
         shell(readFileFromWorkspace('python_1.py'))
     }
